e3374bbdaba7358c371428cec02001a588e29693
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-12-23 22:38:31 +0000

    [master] init project

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://roze-shop/admin/');
+define('HTTP_CATALOG', 'http://roze-shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://roze-shop/admin/');
+define('HTTPS_CATALOG', 'http://roze-shop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/roze-shop/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/roze-shop/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/roze-shop/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/roze-shop/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/roze-shop/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/roze-shop/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/roze-shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/roze-shop/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/roze-shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/roze-shop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/roze-shop/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/roze-shop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'roze-shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCartForum API
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');

afca18f6c63e9739bf613868a0cd560c46070abb
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-12-30 22:55:56 +0000

    [master] global settings of project

--- catalog/view/theme/roze/dev/webpack.config.js
+++ catalog/view/theme/roze/dev/webpack.config.js
@@ -1,3 +1,112 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const IgnoreEmitPlugin = require('ignore-emit-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const path = require('path');
+const glob = require('glob');
+
+const ENV = process.env;
+
+const isWatchMode = ENV.WATCH === 'true';
+
+const PATHS = {
+    src: path.resolve(__dirname, 'src'),
+    src_js: path.resolve(__dirname, 'src/scripts'),
+    src_css: path.resolve(__dirname, 'src/styles'),
+    src_img: path.resolve(__dirname, 'src/images'),
+    public: path.resolve(__dirname, 'public'),
+};
+
+const getEntries = (pattern, extension) => glob
+    .sync(pattern)
+    .reduce((entries, filename) => {
+        const file = filename.split('/').pop();
+        const [name] = file.match(/([a-z-A-Z-0-9]+)(?=\.[a-z]+)/g);
+        const entryName = `${extension}/${name}`;
+        return { ...entries, [entryName]: filename };
+    }, {});
+
+module.exports = () => ({
+    target: ['web', 'es5'],
+    mode: ENV.NODE_ENV,
+    watch: isWatchMode,
+    entry: {
+        ...getEntries(`${PATHS.src_js}/pages/*.js`, 'js'),
+        ...getEntries(`${PATHS.src_js}/utils/*.js`, 'js'),
+        ...getEntries(`${PATHS.src_js}/components/*.js`, 'js'),
+        ...getEntries(`${PATHS.src_css}/pages/*.scss`, 'css'),
+        ...getEntries(`${PATHS.src_css}/components/*.scss`, 'css'),
+    },
+    output: {
+        path: PATHS.public,
+    },
+    devtool: ENV.NODE_ENV === 'development' ? 'source-map' : false,
+    resolve: {
+        alias: {
+            Components: `${PATHS.src_js}/components`,
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\/(?!tippy.js|swiper).*/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /node_modules/,
+                include: PATHS.src_css,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            context: path.resolve(__dirname, 'src/'),
+                            publicPath: '../',
+                            useRelativePaths: true,
+                        },
+                    },
+                ],
+
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new IgnoreEmitPlugin(/(css)\/.*\.(js)/),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: `${PATHS.src_img}`, to: 'images' },
+                { from: `${PATHS.src}/media`, to: 'media' },
+            ],
+        }),
+    ],
+});

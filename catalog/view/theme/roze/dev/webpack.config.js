2fc553aa46b8396b2ba805d42b1b6c20221aaeed
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-12-30 03:13:18 +0000

    [master] add new theme, init webpack

---
+++ catalog/view/theme/roze/dev/webpack.config.js
@@ -0,0 +1,3 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const IgnoreEmitPlugin = require('ignore-emit-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');

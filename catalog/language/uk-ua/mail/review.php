ef47860ae361dc1bbe2088d4573dd53003954d74
Jeckill1993 <evgeniyduplenko@gmail.com>
2023-05-09 09:34:08 +0100

    [master] adding language for admin in progress

--- catalog/language/uk-ua/mail/review.php
+++ catalog/language/uk-ua/mail/review.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_subject']	= '%s - Отзыв Товара';
-$_['text_waiting']	= 'В ожидании новый отзыв товара.';
+$_['text_subject']	= '%s - Відгук Товару';
+$_['text_waiting']	= 'В очікуванні новий відгук товару.';
 $_['text_product']	= 'Товар: %s';
 $_['text_reviewer']	= 'Автор: %s';
 $_['text_rating']	= 'Рейтинг: %s';
-$_['text_review']	= 'Отзыв:';
+$_['text_review']	= 'Відгук:';
--- catalog/language/uk-ua/product/manufacturer.php
+++ catalog/language/uk-ua/product/manufacturer.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Список производителей';
+$_['heading_title']     = 'Список виробників';
 
 // Text
-$_['text_brand']        = 'Производители';
-$_['text_index']        = 'Алфавитный указатель:';
-$_['text_error']        = 'Производитель не найден!';
-$_['text_empty']        = 'Нет товаров';
-$_['text_quantity']     = 'Кол-во:';
-$_['text_manufacturer'] = 'Производитель:';
-$_['text_model']        = 'Код товара:';
-$_['text_points']       = 'Бонусные баллы:';
-$_['text_price']        = 'Цена:';
-$_['text_tax']          = 'Без налога:';
-$_['text_compare']      = 'Сравнение товаров (%s)';
-$_['text_sort']         = 'Сортировать:';
-$_['text_default']      = 'По умолчанию';
-$_['text_name_asc']     = 'По имени (A - Я)';
-$_['text_name_desc']    = 'По имени (Я - A)';
-$_['text_price_asc']    = 'По цене (возрастанию)';
-$_['text_price_desc']   = 'По цене (убыванию)';
-$_['text_rating_asc']   = 'По рейтингу (возрастанию)';
-$_['text_rating_desc']  = 'По рейтингу (убыванию)';
-$_['text_model_asc']    = 'По модели (A - Z)';
-$_['text_model_desc']   = 'По модели (Z - A)';
-$_['text_limit']        = 'Показывать:';
+$_['text_brand']        = 'Виробники';
+$_['text_index']        = 'Алфавітний покажчик:';
+$_['text_error']        = 'Виробник не знайдено!';
+$_['text_empty']        = 'Немає товарів';
+$_['text_quantity']     = 'Кількість:';
+$_['text_manufacturer'] = 'Виробник:';
+$_['text_model']        = 'Код товару:';
+$_['text_points']       = 'Бонусні бали:';
+$_['text_price']        = 'Ціна:';
+$_['text_tax']          = 'Без податку:';
+$_['text_compare']      = 'Порівняння товарів (%s)';
+$_['text_sort']         = 'Сортувати:';
+$_['text_default']      = 'За замовчуванням';
+$_['text_name_asc']     = 'На ім\'я (A - Я)';
+$_['text_name_desc']    = 'На ім\'я (Я - A)';
+$_['text_price_asc']    = 'За ціною (зростанням)';
+$_['text_price_desc']   = 'За ціною (зменшенням)';
+$_['text_rating_asc']   = 'За рейтингом (зростанням)';
+$_['text_rating_desc']  = 'За рейтингом (зменшенням)';
+$_['text_model_asc']    = 'За моделлю (A - Z)';
+$_['text_model_desc']   = 'За моделлю (Z - A)';
+$_['text_limit']        = 'Показувати:';

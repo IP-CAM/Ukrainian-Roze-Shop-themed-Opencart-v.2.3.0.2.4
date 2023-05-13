7939118485459b1fcb2a88b1349717a14460ef8d
Jeckill1993 <evgeniyduplenko@gmail.com>
2023-05-13 17:34:24 +0100

    [master] adding language

--- admin/language/uk-ua/common/header.php
+++ admin/language/uk-ua/common/header.php
@@ -3,21 +3,21 @@
 $_['heading_title']        		= 'ocStore';
 
 // Text
-$_['text_order']          		= 'Заказы';
-$_['text_processing_status']    = 'В процессе';
+$_['text_order']          		= 'Замовлення';
+$_['text_processing_status']    = 'В процесі';
 $_['text_complete_status']      = 'Завершено';
-$_['text_customer']          	= 'Покупатели';
+$_['text_customer']          	= 'Покупці';
 $_['text_online']          		= 'Онлайн';
-$_['text_approval']          	= 'В ожидании';
-$_['text_product']          	= 'Товары';
-$_['text_stock']          		= 'Нет в наличии';
-$_['text_review']          		= 'Отзывы';
-$_['text_return']          		= 'Возвраты';
-$_['text_affiliate']          	= 'Партнерская программа';
-$_['text_store']          		= 'Магазины';
+$_['text_approval']          	= 'В очікуванні';
+$_['text_product']          	= 'Товари';
+$_['text_stock']          		= 'Немає в наявності';
+$_['text_review']          		= 'Відгуки';
+$_['text_return']          		= 'Повернення';
+$_['text_affiliate']          	= 'Партнерська програма';
+$_['text_store']          		= 'Магазини';
 $_['text_front']          		= 'Магазин';
-$_['text_help']          		= 'Помощь';
-$_['text_homepage']          	= 'Сайт проекта';
+$_['text_help']          		= 'Допомога';
+$_['text_homepage']          	= 'Сайт проекту';
 $_['text_support']          	= 'Форум ocStore';
-$_['text_documentation']        = 'Документация';
-$_['text_logout']          		= 'Выход';
+$_['text_documentation']        = 'Документація';
+$_['text_logout']          		= 'Вихід';
--- admin/language/uk-ua/customer/custom_field.php
+++ admin/language/uk-ua/customer/custom_field.php
@@ -1,52 +1,52 @@
 <?php
 // Heading
-$_['heading_title']         = 'Произвольные поля';
+$_['heading_title']         = 'Довільні поля';
 
 // Text
-$_['text_success']          = 'Произвольные поля успешно обновлены!';
-$_['text_list']             = 'Список произвольных полей';
-$_['text_add']              = 'Добавление произвольного поля';
-$_['text_edit']             = 'Редактирование произвольного поля';
-$_['text_choose']           = 'Выбор';
-$_['text_select']           = 'Список (Select)';
-$_['text_radio']            = 'Переключатель (Radio)';
-$_['text_checkbox']         = 'Флажок (Checkbox)';
-$_['text_input']            = 'Ввод';
-$_['text_text']             = 'Текстовая строка (Text)';
-$_['text_textarea']         = 'Текстовое поле (Textarea)';
+$_['text_success']          = 'Довільні поля!';
+$_['text_list']             = 'Список довільних полів';
+$_['text_add']              = 'Додавання довільного поля';
+$_['text_edit']             = 'Редагування довільного поля';
+$_['text_choose']           = 'Вибір';
+$_['text_select']           = 'Перелік (Select)';
+$_['text_radio']            = 'Перемикач (Radio)';
+$_['text_checkbox']         = 'Прапорець (Checkbox)';
+$_['text_input']            = 'Введення';
+$_['text_text']             = 'Текстовий рядок (Text)';
+$_['text_textarea']         = 'Текстове поле (Textarea)';
 $_['text_file']             = 'Файл';
 $_['text_date']             = 'Дата';
-$_['text_datetime']         = 'Дата и время';
-$_['text_time']             = 'Время';
-$_['text_account']          = 'Аккаунт';
-$_['text_address']          = 'Адрес';
-$_['text_regex']           = 'Регулярное выражение';
+$_['text_datetime']         = 'Дата та Час';
+$_['text_time']             = 'Час';
+$_['text_account']          = 'Обліковий запис';
+$_['text_address']          = 'Адреса';
+$_['text_regex']           = 'Регулярний вираз';
 
 // Column
-$_['column_name']           = 'Название';
-$_['column_location']       = 'Где показано';
+$_['column_name']           = 'Назва';
+$_['column_location']       = 'Де показано';
 $_['column_type']           = 'Тип поля';
-$_['column_sort_order']     = 'Порядок сортировки';
-$_['column_action']         = 'Действие';
+$_['column_sort_order']     = 'Порядок сортування';
+$_['column_action']         = 'Дія';
 
 // Entry
-$_['entry_name']            = 'Название';
-$_['entry_location']        = 'Где показывать';
+$_['entry_name']            = 'Назва';
+$_['entry_location']        = 'Де показувати';
 $_['entry_type']            = 'Тип поля';
-$_['entry_value']           = 'Значение';
-$_['entry_validation']     = 'Проверка';
-$_['entry_custom_value']    = 'Значение для этого поля';
-$_['entry_customer_group']  = 'Группа покупателей';
-$_['entry_required']        = 'Обязательно';
+$_['entry_value']           = 'Значення';
+$_['entry_validation']     = 'Перевірка';
+$_['entry_custom_value']    = 'Значення для цього поля';
+$_['entry_customer_group']  = 'Група покупців';
+$_['entry_required']        = 'Обов\'язково';
 $_['entry_status']          = 'Статус';
-$_['entry_sort_order']      = 'Порядок сортировки';
+$_['entry_sort_order']      = 'Порядок сортування';
 
 // Help
-$_['help_regex']           = 'Использовать регулярное выражение. Т.е: /[a-zA-Z0-9_-]/';
-$_['help_sort_order']       = 'Используйте - (минус) что бы просто скрыть поле из вида.';
+$_['help_regex']           = 'Використовувати регулярний вираз. Тобто: /[a-zA-Z0-9_-]/';
+$_['help_sort_order']       = 'Використовуйте - (мінус) щоб просто приховати поле з виду.';
 
 // Error
-$_['error_permission']      = 'Внимание: у вас нет прав на изменение произвольных полей!';
-$_['error_name']            = 'Название поля должно быть от 1 до 128 символов!';
-$_['error_type']            = 'Необходимо указать значение!';
-$_['error_custom_value']    = 'Значение должно быть от 1 до 128 символов!';
+$_['error_permission']      = 'Увага: у вас немає прав на зміну довільних полів!';
+$_['error_name']            = 'Назва поля має бути від 1 до 128 символів!';
+$_['error_type']            = 'Необхідно вказати значення!';
+$_['error_custom_value']    = 'Значення має бути від 1 до 128 символів!';
